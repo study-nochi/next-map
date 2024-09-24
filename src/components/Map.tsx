e50621b428fe7e4fd3a64bcf85c17be29a454947
Nochi <kthrkdals@gmail.com>
2024-09-24 21:06:18 +0900

    feat: 마커 커스텀 이미지 적용

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -23,17 +23,37 @@ export default function Map() {
       };
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
+      // const types = stores.DATA.map((store) => store.bizcnd_code_nm).reduce((acc, cur) => {
+      //   if (cur && !acc.includes(cur)) {
+      //     acc.push(cur);
+      //   }
+      //   return acc;
+      // }, [] as string[]);
+      // console.log(types);
+
       // 식당 데이터 마커 띄우기
       stores.DATA.map((store) => {
-        // 마커가 표시될 위치입니다
-        var markerPosition = new window.kakao.maps.LatLng(
-          store?.y_dnts,
-          store?.x_cnts
-        );
+        const imageSrc = store?.bizcnd_code_nm
+            ? `/images/markers/${store.bizcnd_code_nm}.png`
+            : "/images/markers/default.png", // 마커이미지의 주소입니다
+          imageSize = new window.kakao.maps.Size(40, 40), // 마커이미지의 크기입니다
+          imageOption = { offset: new window.kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+        // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+        const markerImage = new window.kakao.maps.MarkerImage(
+            imageSrc,
+            imageSize,
+            imageOption
+          ),
+          markerPosition = new window.kakao.maps.LatLng(
+            store?.y_dnts,
+            store?.x_cnts
+          ); // 마커가 표시될 위치입니다
 
         // 마커를 생성합니다
-        var marker = new window.kakao.maps.Marker({
+        const marker = new window.kakao.maps.Marker({
           position: markerPosition,
+          image: markerImage, // 마커이미지 설정
         });
 
         // 마커가 지도 위에 표시되도록 설정합니다
